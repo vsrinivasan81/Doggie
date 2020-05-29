7b02004d95fe5b76a92701ebe2dd9efe781e1362
Vijay Srinivasan <cbevijay@gmail.com>
2020-05-29 17:38:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/dogs/LaunchScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.example.android.dogs
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchScreenActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val activityIntent = Intent(this, MainActivity::class.java)
+        startActivity(activityIntent)
+        finish()
+    }
+
+
+}
