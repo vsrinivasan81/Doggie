a4c571479b1c9b3daa032a20c982f8d6af911c0c
Vijay Srinivasan <cbevijay@gmail.com>
2020-05-29 18:03:42 +0530

    Enabled data binding and added sample code to set welcome text

--- app/src/main/java/com/example/android/dogs/MainActivity.kt
+++ app/src/main/java/com/example/android/dogs/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.android.dogs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.android.dogs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    data class Welcome (var helloText: String = "")
+    private lateinit var binding: ActivityMainBinding
+    private var welcomeObject:Welcome = Welcome("Dog Lovers, Welcome !!!")
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.welcomeObj = welcomeObject
     }
 }
